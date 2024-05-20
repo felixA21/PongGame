0f7897cfc39b4dce161fb9d6f2555c6c62a324ff
Felix Alimin <felixalimin@gmail.com>
2024-05-19 21:10:32 -0400

    Added miss paddle on ball logic

--- main.py
+++ main.py
@@ -36,6 +36,12 @@ while(game_is_one):
             ball.distance(l_paddle) <50 and ball.xcor() < -320):
         ball.bounce_x()
 
+#         Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
 
+#         Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
